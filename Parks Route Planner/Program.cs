798daf5edac5011219ee88a18d8cd327487cf2e0
Cryptic Realms <113389247+Chris-Greniewicki@users.noreply.github.com>
2026-06-13 16:01:59 -0500

    Refactored to include zones in each park's identity

--- Parks Route Planner/Program.cs
+++ Parks Route Planner/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+string filePath = "config.json";
+string jsonString = File.ReadAllText(filePath);
+
+Console.WriteLine("Hello, World!");
