97336befc413d59d2bb63aaeff6136595e16604e
Cryptic Realms <113389247+Chris-Greniewicki@users.noreply.github.com>
2026-06-14 15:07:46 -0500

    Notation of tests

--- Parks Route Planner/Program.cs
+++ Parks Route Planner/Program.cs
@@ -7,11 +7,20 @@ string currentDir = AppContext.BaseDirectory;
 string filePath = Path.Combine(currentDir, "config.json");
 string jsonString = File.ReadAllText(filePath);
 
-//Need to figure out class creation before this section will work
 Config config = JsonSerializer.Deserialize<Config>(jsonString);
 
+//test of basic data retrieval
 Console.WriteLine($"{config.Crews} Crews found");
 Console.WriteLine($"{config.NextMowEventDate} set as next mow event date");
 Console.WriteLine($"{config.Zones.Count} Zones found");
+//test of ability to count parks across zones
 int parkCount = config.Zones.Sum(Zone => Zone.Parks.Count);
 Console.WriteLine($"{parkCount} Parks found");
+//testing simple enum selection
+Console.WriteLine($"{Crew.Crew2} selected");
+//testing ability to set properties of a new object
+Assignment test = new Assignment();
+test.AssignedCrew = Crew.Crew2;
+Console.WriteLine($"{test.AssignedCrew} assigned");
+//Console.WriteLine($"{}");
+//Console.WriteLine($"{}");
