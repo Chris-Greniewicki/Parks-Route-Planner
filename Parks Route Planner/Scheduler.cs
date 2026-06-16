c1eb65000d5ad79f00c34d75ebb525531d66deff
Chris Greniewicki <ccgreniewicki@gmail.com>
2026-06-16 14:39:42 -0500

    Created random zone selection based on the zone still containing uncut parks Added notations

--- Parks Route Planner/Scheduler.cs
+++ Parks Route Planner/Scheduler.cs
@@ -6,6 +6,7 @@ namespace Parks_Route_Planner
 {
     internal class Scheduler
     {
+        //Creates a dictionary of all possible crew pairings
         public Dictionary<string, int> crewPairs = new()
         {
             ["1-2"] = 0,
@@ -15,6 +16,7 @@ namespace Parks_Route_Planner
             ["2-4"] = 0,
             ["3-4"] = 0
         };
+        //Creates a list of parks for each zone that can be manipulated during the 2 week period to determine assignments
         public Dictionary<int, List<Site>> remainingParks = new();
 
         public List<Zone> zones = new();
@@ -28,6 +30,25 @@ namespace Parks_Route_Planner
             }
         }
 
-
+        //Generates a list of available zones based on whether parks are left to do or not and randomly selects 2 zones and returns them
+        internal List<int> PickZones()
+        {
+            List<int> availableZones = new();
+            foreach (KeyValuePair<int, List<Site>> zone in remainingParks)
+            {
+                if (zone.Value.Count > 0)
+                {
+                    Console.WriteLine($"Zone {zone.Key} is available");
+                    availableZones.Add(zone.Key);
+                }
+                else
+                {
+                    Console.WriteLine("Nothing in Zones");
+                }
+            }
+            List<int> selectedZones = new();
+            selectedZones = Random.Shared.GetItems(availableZones.ToArray(), 2).ToList();
+            return selectedZones;
+        }
     }
 }
