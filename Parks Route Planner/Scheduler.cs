6db0a58ff346151771c451bbbcbe0d44ecd4d73b
Chris Greniewicki <ccgreniewicki@gmail.com>
2026-06-16 13:41:05 -0500

    Set up scheduler.cs to begin populating lists of parks for each zone in a temporary list that can be modified day by day

---
+++ Parks Route Planner/Scheduler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parks_Route_Planner
+{
+    internal class Scheduler
+    {
+        public Dictionary<string, int> crewPairs = new()
+        {
+            ["1-2"] = 0,
+            ["1-3"] = 0,
+            ["1-4"] = 0,
+            ["2-3"] = 0,
+            ["2-4"] = 0,
+            ["3-4"] = 0
+        };
+        public Dictionary<int, List<Site>> remainingParks = new();
+
+        public List<Zone> zones = new();
+
+        public Scheduler(List<Zone> zoneList)
+        {
+            zones = zoneList;
+            foreach (Zone zone in zoneList)
+            {
+                remainingParks.Add(zone.ZoneId, new List<Site>(zone.Parks));
+            }
+        }
+    }
+}
