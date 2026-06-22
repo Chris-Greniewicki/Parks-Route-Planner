82fee1a6a3bfd1f6f3e5bd9edec074442abd7043
Chris Greniewicki <ccgreniewicki@gmail.com>
2026-06-22 14:35:05 -0500

    Removed .bat Program is now self contained and no longer needs additional dependencies

--- Parks Route Planner/Launch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-dotnet --version >nul 2>&1
-if %errorlevel% neq 0 (
-    echo .NET 10 Runtime not found. Installing now...
-    dotnet-sdk-10.0.301-win-x64.exe /quiet /norestart
-    echo Installation complete. Launching Parks Route Planner...
-    timeout /t 3 >nul
-)
-start "" "Parks Route Planner.exe"
